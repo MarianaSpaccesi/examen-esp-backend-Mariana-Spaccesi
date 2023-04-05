ad7691b56a3422995f21c71cad6e1d8b3e073282
Mariana Spaccesi <marianaism.smi@gmail.com>
2023-04-05 17:00:21 -0300

    añado configuración de feing de Series

--- api-catalog/src/main/java/com/dh/catalog/client/MovieServiceClient.java
+++ api-catalog/src/main/java/com/dh/catalog/client/MovieServiceClient.java
@@ -1,9 +1,7 @@
 package com.dh.catalog.client;
 
-import com.dh.catalog.config.LoadBalancerConfiguration;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 @FeignClient(name="api-movie")
-@LoadBalancerClient(value="api-movie", configuration= LoadBalancerConfiguration.class)
 public interface MovieServiceClient {
 
 	@GetMapping("/api/v1/movies/{genre}")
